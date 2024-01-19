2a5f63163e021ffe86c20b1eba3fe03f45d5ee3b
DavidCZ2051 <84991177+DavidCZ2051@users.noreply.github.com>
2024-01-19 15:25:45 +0100

    Marker clusters

--- client/isse_app/lib/routes/map.dart
+++ client/isse_app/lib/routes/map.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'dart:math' as math;
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -32,13 +34,31 @@ class _MapPageState extends State<MapPage> {
               userAgentPackageName: 'dev.fleaflet.flutter_map.example',
               tileProvider: CancellableNetworkTileProvider(),
             ),
-            const MarkerLayer(
-              markers: [
-                Marker(
-                  point: LatLng(50.1071528, 14.4502697),
-                  child: Icon(Icons.location_on),
-                ),
-              ],
+            MarkerClusterLayerWidget(
+              options: MarkerClusterLayerOptions(
+                maxClusterRadius: 45,
+                size: const Size(40, 40),
+                markers: [
+                  for (int i = 0; i < 100; i++)
+                    Marker(
+                      point: LatLng(50.1071528 + i / 1000, 14.4502697),
+                      child: const Icon(Icons.location_on),
+                    ),
+                ],
+                builder: (context, markers) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blue),
+                    child: Center(
+                      child: Text(
+                        markers.length.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
