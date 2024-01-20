9306a382a349a6d55d52184fee6b46fdc5c4f3c0
DavidCZ2051 <84991177+DavidCZ2051@users.noreply.github.com>
2024-01-20 09:14:04 +0100

    independent app

---
+++ client/app_client_independent/android/app/src/main/kotlin/com/example/app_client_independent/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_client_independent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ client/app_client_independent/lib/routes/view.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class ViewPage extends StatefulWidget {
+  const ViewPage({super.key});
+
+  @override
+  State<ViewPage> createState() => _ViewPageState();
+}
+
+class _ViewPageState extends State<ViewPage> {
+  int selectedPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.home),
+            SizedBox(width: 8),
+            Text("Společenství Liberec - Skupina 1 - Příjemce"),
+            const SizedBox(
+              width: 16,
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, "/view2");
+              },
+              child: Text("Přepnout na Výdejce"),
+            ),
+          ],
+        ),
+      ),
+      body: Row(
+        children: [
+          NavigationDrawer(
+            children: [
+              ListTile(
+                title: const Text("Historie spotřeby"),
+                leading: const Icon(Icons.history),
+                selected: selectedPage == 0,
+                onTap: selectedPage == 0
+                    ? null
+                    : () {
+                        setState(() {
+                          selectedPage = 0;
+                        });
+                      },
+              ),
+              ListTile(
+                title: const Text("Nastavení spotřeby"),
+                leading: const Icon(Icons.settings),
+                selected: selectedPage == 1,
+                onTap: selectedPage == 1
+                    ? null
+                    : () {
+                        setState(() {
+                          selectedPage = 1;
+                        });
+                      },
+              ),
+            ],
+          ),
+          Expanded(
+            child: IndexedStack(
+              index: selectedPage,
+              children: const [
+                Placeholder(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
